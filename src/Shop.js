0bdde47b1ee91ee5d84236ab3502c05752781e27
Felipe Marcon <felipe@growthhackers.com>
2020-03-15 17:27:06 -0300

    Fetch data from open api and create dynamic route links

--- src/Shop.js
+++ src/Shop.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function Shop() {
+
+  useEffect(() => {
+    fetchItems();
+  }, [])
+
+  const [items, setItems] = useState([]);
+
+  const fetchItems = async () => {
+    const data = await fetch(
+      'https://fortnite-api.theapinetwork.com/store/get'
+    );
+
+    const itemsObject = await data.json();
+    console.log(itemsObject.data);
+    setItems(itemsObject.data);
+  };
+
   return (
     <div>
-      <h1>Shop Page</h1>
+      {items.map(item => (
+        <h1 key={item.itemId}>
+          <Link to={`/shop/${item.itemId}`}>{item.item.name}</Link>
+        </h1>
+      ))}
     </div>
   );
 }
